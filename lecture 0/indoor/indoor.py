47e824382de718328977a7bc454a81dc9e8169e1
Ndong Henry <ndonghenryndang@gmail.com>
2025-10-03 16:39:52 +0100

    Added all solutions to the problem set of lecture 0

---
+++ lecture 0/indoor/indoor.py
@@ -0,0 +1,7 @@
+def to_lowercase() -> str:
+    word: str = input("Enter a string to convert to lower case ")
+
+    return word.lower()
+
+
+print(to_lowercase())
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def main():
+def main() -> None:
     print("Hello from cs50p-solutions-to-problem-set!")
 
 
